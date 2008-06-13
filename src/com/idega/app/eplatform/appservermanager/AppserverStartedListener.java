d6d3232078bc687fa5a20fe52e8e7a19be1c834f
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2008-06-13 10:28:18 +0000

    refactgoring to support more instances

--- src/com/idega/app/eplatform/appservermanager/AppserverStartedListener.java
+++ src/com/idega/app/eplatform/appservermanager/AppserverStartedListener.java
@@ -2,6 +2,6 @@ package com.idega.app.eplatform.appservermanager;
 
 public interface AppserverStartedListener {
 
-	public void notifyStarted();
+	public void notifyStarted(AppserverInstance startedInstance);
 	
 }
